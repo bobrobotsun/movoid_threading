e64e47042332bcd50569c0eb4802996258e73e53
movoid <bobrobotsun@163.com>
2024-04-11 23:44:36 +0800

    Initial commit

---
+++ movoid_threading/__init__.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+# File          : __init__
+# Author        : Sun YiFan-Movoid
+# Time          : 2024/4/3 1:49
+# Description   : 
+"""
+
+if __name__ == '__main__':
+    pass
