e8dd62080b7757d6b930541f70a7028980646c56
bobrobotsun <bobrobotsun@163.com>
2024-08-22 01:33:56 +0800

    2024.08.21.01-movoid dev 可以触发github test

---
+++ test/__init__.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+# File          : __init__
+# Author        : Sun YiFan-Movoid
+# Time          : 2024/8/22 1:31
+# Description   : 
+"""
+
+if __name__ == '__main__':
+    pass
